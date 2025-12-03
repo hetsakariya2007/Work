3fbde06251ed0032fae2295e415f2cd59c1a14bd
hetsakariya2007 <het.sakariya.cg@gmail.com>
2025-12-03 10:31:24 +0530

    Frontend

---
+++ C_Lang/3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a;
+    a[7]={20,32,6,7,22,47,21};
+    for(int i=1; i<=a[].length; i++){
+
+    
+    if(a[i]>a[i+1]){
+        printf("%d is the largest num of the array.");
+    }
+    else{
+        printf("%d is the smallest num of the array");
+    }
+}
+
+    return 0;
+}
---
+++ C_Lang/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    arr[3]={"het"};
+
+    arr[i]==arr[n] && arr[n]==arr[i];
+
+    printf("%s",arr[]);
+
+    return 0;
+}
